610c42ceb55ff01b035155d429ab17132b879cb4
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-16 22:42:27 +0300

    added noise with random util in for loop

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 // these declarations set the size and dimensions within the grid
-    const cols = 4;
-    const rows = 3;
+    const cols = 10;
+    const rows = 10;
     const numCells = cols * rows;
 
     const gridw = width * 0.8;
@@ -29,11 +29,16 @@ const sketch = () => {
       const y = row * cellh;
       const w = cellw * 0.8;
       const h = cellh * 0.8;
+      // add noise
+      const n = random.noise2D(x,y);
+      const angle = n * Math.PI;
+
       //set position in cell
       context.save();
       context.translate(x,y);
       context.translate(marginx, marginy);
       context.translate(cellw * 0.5, cellh * 0.5);
+      context.rotate(angle);
 
       context.lineWidth =  4;
       //start the line drawing
