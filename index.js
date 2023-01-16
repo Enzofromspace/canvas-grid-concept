22783279a4bdf0746cec7234e5e1050dd34382d2
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-16 22:17:50 +0300

    set grid with declarations

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+// these declarations set the size and dimensions within the grid
+    const cols = 4;
+    const rows = 3;
+    const numCells = cols * rows;
+
+    const gridw = width * 0.8;
+    const gridh = height * 0.8
+    const cellw = gridw / cols;
+    const cellh = gridh / rows;
+    const marginx = (width - gridw) * 0.5;
+    const marginy = (height - gridh) * 0.5; //set margins around grid
+
   };
 };
 
